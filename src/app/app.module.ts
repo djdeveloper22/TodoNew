319a7bc4c838c98cbb52dba39f2e82faa35b77ba
djdeveloper22 <115105022+djdeveloper22@users.noreply.github.com>
2023-03-25 22:36:31 -0500

    se maqueta el aplicativo todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './ui/task/task.component';
+import { ListTaskComponent } from './ui/list-task/list-task.component';
+import { ModalComponent } from './shared/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    ListTaskComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
